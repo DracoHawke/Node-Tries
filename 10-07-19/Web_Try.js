e61e203caeb40e90d010af3f569a78c1333706e1
Tanuj Goyal <tanuj.goyal@satgurutechnologies.com>
2019-07-10 18:09:53 +0530

    first day

--- 10-07-19/Web_Try.js
+++ 10-07-19/Web_Try.js
@@ -18,8 +18,29 @@ http.createServer(function (req, res) {
     return res.end();
   });
   fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
-  if (err) throw err;
-  console.log('Saved!');
-});
+    if (err) throw err;
+    console.log('Saved!');
+  });
+  // way of updating file, appends data at the end of the file. Also creates new file if file already not present.
+  fs.open('mynewfile2.txt', 'w', function (err, file) {
+    if (err) throw err;
+    console.log('Saved!');
+  });
+  // takes a "flag" as the second argument, if the flag is "w" for "writing", it is opened for writing. An empty file is created if file not present.
+  fs.writeFile('mynewfile3.txt', 'Hello content!', function (err) {
+    if (err) throw err;
+    console.log('Saved!');
+  });
+  // way of updating file, replaces all information with new info. Also creates new file if file already not present and writes the data.
+  fs.unlink('mynewfile2.txt', function (err) {
+    if (err) throw err;
+    console.log('File deleted!');
+  });
+  // The fs.unlink() method deletes the specified file.
+  fs.rename('mynewfile1.txt', 'myrenamedfile.txt', function (err) {
+    if (err) throw err;
+    console.log('File Renamed!');
+  });
+  // The fs.rename() method renames the specified file.
   res.write(' Hello World!');
 }).listen(8080);
